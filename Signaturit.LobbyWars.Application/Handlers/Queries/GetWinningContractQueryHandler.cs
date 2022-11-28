6314800f73b10c28cd9971dcba1cb0d71aa8a4d9
hirandiaz <hirandiaz266@gmail.com>
2022-11-28 02:28:28 +0100

    Add GetWinningContract code

--- Signaturit.LobbyWars.Application/Handlers/Queries/GetWinningContractQueryHandler.cs
+++ Signaturit.LobbyWars.Application/Handlers/Queries/GetWinningContractQueryHandler.cs
@@ -2,19 +2,50 @@
 using Signaturit.LobbyWars.Core.Data.Queries.Handlers;
 using Signaturit.LobbyWars.Domain.Data.Queries;
 using Signaturit.LobbyWars.Domain.Entities;
+using Signaturit.LobbyWars.Domain.Services.Base;
+using Signaturit.LobbyWars.Domain.Specifications;
 
 namespace Signaturit.LobbyWars.Application.Handlers.Queries
 {
     public class GetWinningContractQueryHandler : QueryHandler<GetWinningContractQuery, Contract>
     {
-        public override Task<QueryResult<Contract>> Handle(GetWinningContractQuery query, CancellationToken cancellationToken)
+        private readonly IGetContractValueService _getContractValueService;
+
+        public GetWinningContractQueryHandler(IGetContractValueService getContractValueService) : base()
+        {
+            _getContractValueService = getContractValueService;
+        }
+
+
+        public override async Task<QueryResult<Contract>> Handle(GetWinningContractQuery query, CancellationToken cancellationToken)
         {
             if (query.Parameters is not { } || !query.Parameters.Any())
             {
                 throw new InvalidOperationException("No parameters found");
             }
 
-            throw new NotImplementedException();
+            if (query.Parameters.Count == 1 && query.Parameters.TryGetValue("contract1", out var obj) &&
+                obj is Contract contract)
+            {
+                int contractValue = _getContractValueService.GetValue(contract);
+
+                return contractValue < 0 ?
+                    new QueryResult<Contract>(Array.Empty<Contract>()) :
+                    new QueryResult<Contract>(new[] { contract });
+            }
+
+            if (query.Parameters.Count == 2 && query.Parameters.TryGetValue("contract1", out var obj1) &&
+                obj1 is Contract contract1 && query.Parameters.TryGetValue("contract2", out var obj2) &&
+                obj2 is Contract contract2)
+            {
+                int contractValue1 = _getContractValueService.GetValue(contract1);
+                int contractValue2 = _getContractValueService.GetValue(contract2);
+
+                return contractValue1 >= contractValue2
+                    ? new QueryResult<Contract>(new[] { contract1 })
+                    : new QueryResult<Contract>(new[] { contract2 });
+            }
+            throw new InvalidOperationException("only two contracts can be analyzed at most at the same time");
         }
     }
 }
--- Signaturit.LobbyWars.Test/GetWinningContractShould.cs
+++ Signaturit.LobbyWars.Test/GetWinningContractShould.cs
@@ -3,7 +3,12 @@ using Moq;
 using Signaturit.LobbyWars.Application.Handlers.Queries;
 using Signaturit.LobbyWars.Core.Data.Routers;
 using Signaturit.LobbyWars.Domain.Data.Queries;
+using Signaturit.LobbyWars.Domain.Entities;
+using Signaturit.LobbyWars.Domain.Factories;
+using Signaturit.LobbyWars.Domain.Services;
+using Signaturit.LobbyWars.Domain.Services.Base;
 using System;
+using System.Collections.Generic;
 using System.Threading;
 using Xunit;
 
@@ -17,7 +22,9 @@ namespace Signaturit.LobbyWars.Test
             //Arrange
             Mock<IMediator>? mockMediator = new();
 
-            GetWinningContractQueryHandler queryHandler = new();
+            Mock<IGetContractValueService> mockService = new();
+
+            GetWinningContractQueryHandler queryHandler = new(mockService.Object);
 
             mockMediator
               .Setup(m => m.Send(It.IsAny<GetWinningContractQuery>(), It.IsAny<CancellationToken>()))
@@ -33,7 +40,51 @@ namespace Signaturit.LobbyWars.Test
             var action = async () => await queryRouter.QueryOneAsync(query).ConfigureAwait(false);
 
             //Assert
-           await Assert.ThrowsAsync<InvalidOperationException>(action);
+            await Assert.ThrowsAsync<InvalidOperationException>(action);
+
+        }
+
+        [Theory]
+        [InlineData(5, 7, 7)]
+        [InlineData(8, 6, 8)]
+        public async void Handle_IfPass2Contract_ReturnExpectedValue(
+            int contractValue1, int contractValue2, int expectedValue)
+        {
+            //Arrange
+            Mock<Contract> contract1 = new();
+            Mock<Contract> contract2 = new();
+
+            Mock<IMediator>? mockMediator = new();
+
+            Mock<IGetContractValueService> mockService = new();
+
+            mockService.Setup(service => service.GetValue(contract1.Object)).Returns(contractValue1);
+
+            mockService.Setup(service => service.GetValue(contract2.Object)).Returns(contractValue2);
+
+            GetWinningContractQueryHandler queryHandler = new(mockService.Object);
+
+            mockMediator
+              .Setup(m => m.Send(It.IsAny<GetWinningContractQuery>(), It.IsAny<CancellationToken>()))
+
+              .Returns<GetWinningContractQuery, CancellationToken>(
+                      async (query, token) => await queryHandler.Handle(query, token));
+
+            QueryRouter queryRouter = new(mockMediator.Object);
+
+            GetWinningContractQuery query = new();
+
+            query.AddParameter("contract1", contract1.Object)
+                .AddParameter("contract2", contract2.Object);
+
+            //Act
+            Contract contract = await queryRouter.QueryOneAsync(query).ConfigureAwait(false);
+
+            int contractValue = mockService.Object.GetValue(contract);
+
+            //Assert
+            //Assert.Same(expected: contract2.Object, contract);
+            Assert.Equal(expected: expectedValue, contractValue);
 
         }
     }
