5523024fc32ec3001280703eb45202c5186a47b6
hirandiaz <hirandiaz266@gmail.com>
2022-11-29 22:56:29 +0100

    ADD GeMinimumSignature endpoint

--- Signaturit.LobbyWars.Api/Program.cs
+++ Signaturit.LobbyWars.Api/Program.cs
@@ -69,6 +69,43 @@ app.MapPost("/winningContract", async (ContractInputVm inputVm, ICommandRouter c
 })
 .WithName("GeWinningContract");
 
+app.MapPost("/minimumSignature", async (ContractInputVm inputVm, ICommandRouter commandRouter, IQueryRouter queryRouter) =>
+{
+    var (signatures1, signatures2) = inputVm;
+
+    IEnumerable<SignatureRole> signatureRoles1 = SignaturitMapper.MapToSignatureRole(signatures1);
+    IEnumerable<SignatureRole> signaturesRoles2 = SignaturitMapper.MapToSignatureRole(signatures2);
+
+    CreateContractCommand createContract1 = new(new SignatureCollectionDto(signatureRoles1));
+    CreateContractCommand createContract2 = new(new SignatureCollectionDto(signaturesRoles2));
+
+    CommandResult<Contract> commandResult1 = await commandRouter.ExecuteAsync(createContract1);
+    CommandResult<Contract> commandResult2 = await commandRouter.ExecuteAsync(createContract2);
+
+    if (commandResult1.IsSuccess && commandResult2.IsSuccess)
+    {
+        Contract contract1 = commandResult1.Item;
+        Contract contract2 = commandResult2.Item;
+
+        GetMinimumSignatureQuery query = new();
+
+        query
+        .AddParameter("contract1", contract1)
+        .AddParameter("contract2", contract2);
+
+        Signature signature = await queryRouter.QueryOneAsync(query).ConfigureAwait(false);
+
+        if (signature is { })
+        {
+            char signatureStr = SignaturitMapper.MapToChar(signature.Role);
+
+            return Results.Ok(signatureStr);
+        }
+    }
+    return Results.BadRequest();
+})
+.WithName("GetMinimumSignature");
+
 app.Run();
 
 
