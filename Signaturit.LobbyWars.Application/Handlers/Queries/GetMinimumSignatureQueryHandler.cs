975805477814826fc0e1cd8e1ee8ed6f2e5f6198
hirandiaz <hirandiaz266@gmail.com>
2022-11-29 17:00:42 +0100

    check query parameters

---
+++ Signaturit.LobbyWars.Application/Handlers/Queries/GetMinimumSignatureQueryHandler.cs
@@ -0,0 +1,22 @@
+﻿using Signaturit.LobbyWars.Core.Data.Models;
+using Signaturit.LobbyWars.Core.Data.Queries.Handlers;
+using Signaturit.LobbyWars.Domain.Data.Queries;
+using Signaturit.LobbyWars.Domain.Entities;
+
+namespace Signaturit.LobbyWars.Application.Handlers.Queries
+{
+    public class GetMinimumSignatureQueryHandler : QueryHandler<GetMinimumSignatureQuery, Signature>
+    {
+        public GetMinimumSignatureQueryHandler()
+        {
+        }
+
+        public override async Task<QueryResult<Signature>> Handle(GetMinimumSignatureQuery query, CancellationToken cancellationToken)
+        {
+            if (query.Parameters is null || query.Parameters.Count < 2)
+                throw new InvalidOperationException("parameters not found");
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Signaturit.LobbyWars.Test/GeMinimumSignatureQueryShould.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using Moq;
+using Signaturit.LobbyWars.Application.Handlers.Queries;
+using Signaturit.LobbyWars.Core.Data.Routers;
+using Signaturit.LobbyWars.Domain.Data.Queries;
+using Signaturit.LobbyWars.Domain.Services.Base;
+using System;
+using System.Threading;
+using Xunit;
+
+namespace Signaturit.LobbyWars.Test
+{
+    public class GeMinimumSignatureQueryShould
+    {
+        [Fact]
+        public async void Handle_IfNumberIsLessThanTwo_ThrowInvalidOperation()
+        {
+            //Arrange
+            Mock<IMediator>? mockMediator = new();
+
+            Mock<IGetContractValueService> mockService = new();
+
+            GetMinimumSignatureQueryHandler queryHandler = new();
+
+            mockMediator
+              .Setup(m => m.Send(It.IsAny<GetMinimumSignatureQuery>(), It.IsAny<CancellationToken>()))
+
+              .Returns<GetMinimumSignatureQuery, CancellationToken>(
+                      async (query, token) => await queryHandler.Handle(query, token));
+
+            QueryRouter queryRouter = new(mockMediator.Object);
+
+            GetMinimumSignatureQuery query = new();
+
+            //Act
+            var action = async () => await queryRouter.QueryOneAsync(query).ConfigureAwait(false);
+
+            //Assert
+            await Assert.ThrowsAsync<InvalidOperationException>(action);
+        }
+    }
+}
