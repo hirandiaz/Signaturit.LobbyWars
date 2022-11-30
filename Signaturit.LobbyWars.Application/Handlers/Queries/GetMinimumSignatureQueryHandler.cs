55d68573678e377fbc932f7e7986e24484b40dda
hirandiaz <hirandiaz266@gmail.com>
2022-11-30 17:53:41 +0100

    add swap method

--- Signaturit.LobbyWars.Application/Handlers/Queries/GetMinimumSignatureQueryHandler.cs
+++ Signaturit.LobbyWars.Application/Handlers/Queries/GetMinimumSignatureQueryHandler.cs
@@ -29,6 +29,9 @@ namespace Signaturit.LobbyWars.Application.Handlers.Queries
                 query.Parameters.TryGetValue("contract1", out var obj1) && obj1 is Contract contract1 &&
                 query.Parameters.TryGetValue("contract2", out var obj2) && obj2 is Contract contract2)
             {
+
+                (contract1, contract2) = Swap(contract1, contract2);
+
                 int contractValue2 = _getContractValueService.GetValue(contract2);
 
                 SignatureRole signatureRole = SignatureRole.Missing;
@@ -44,6 +47,17 @@ namespace Signaturit.LobbyWars.Application.Handlers.Queries
             }
 
             throw new InvalidOperationException("only two contracts can be analyzed at most at the same time");
+        }
+
+        private (Contract, Contract) Swap(Contract contract1, Contract contract2)
+        {
+            if (contract1.MissingSignature)
+                return (contract1, contract2);
+
+            if (contract2.MissingSignature)
+                return (contract2, contract1);
+
+            throw new InvalidOperationException("No missing signature found");
         }
     }
 }
